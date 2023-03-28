b61ca6fd7009b511f85750f8c4518d4ac12a35f7
tazeek <tazeek.rakib@gmail.com>
2023-03-28 18:40:30 +0800

    More reading done

--- utils/load.py
+++ utils/load.py
@@ -50,14 +50,17 @@ class DataHub(object):
         
     @classmethod
     def from_dir_addadj(cls, dir_path):
+
         house = DataHub()
 
         house._data_collection["train"] = house._read_data(
             os.path.join(dir_path, "train.json"), True
         )
+
         house._data_collection["dev"] = house._read_data(
             os.path.join(dir_path, "dev.json"), False
         )
+
         house._data_collection["test"] = house._read_data(
             os.path.join(dir_path, "test.json"), False
         ) 
@@ -65,6 +68,7 @@ class DataHub(object):
 
     @staticmethod
     def _read_adj(file_path: str):
+
         lines = load_txt(file_path)
         adjs = []
         adjs_full_connect = []
@@ -73,24 +77,32 @@ class DataHub(object):
 
         # Init adjacency matrix for each dialog
         for line in lines:
+
             if len(line) == 0:
                 continue
+
             length = int(line.split()[0])
             edges = line.split()[1:]
+
             adj = [[0] * length for _ in range(length)]
             adj_I = [[0] * length for _ in range(length)]
+
             for i in range(length):
                 adj_I[i][i] = 1
             adj_full = [[1] * length for _ in range(length)]
+
             for edge in edges:
                 i, j = edge.split("-")
                 adj[int(i)-1][int(j)-1] = 1
                 adj[int(j)-1][int(i)-1] = 1
+
             adjs.append(adj)
+
             # full connect adj
             adjs_full_connect.append(adj_full)
             # Identity matrix
             adjs_I.append(adj_I)
+
         return adjs, adjs_full_connect, adjs_I
 
     def _read_data(self,
@@ -107,6 +119,7 @@ class DataHub(object):
             utt, emotion, act = [], [], []
 
             for interact in session:
+
                 act.append(interact["act"])
                 emotion.append(interact["sentiment"])
 
@@ -117,6 +130,7 @@ class DataHub(object):
             sent_list.append(emotion)
             act_list.append(act)
 
+        # For adjacency matrix creation
         adjpath = file_path.split(".")[0] + "_adj.txt"
         adj_list, adj_full_list, adj_I_list = self._read_adj(adjpath)
         
@@ -132,17 +146,20 @@ class DataHub(object):
         return utt_list, sent_list, act_list, adj_list, adj_full_list, adj_I_list
 
     def get_iterator(self, data_name, batch_size, shuffle):
+
         data_set = _GeneralDataSet(*self._data_collection[data_name])
 
         data_loader = DataLoader(
             data_set, batch_size, shuffle, collate_fn=_collate_func
         )
+
         return data_loader
 
 
 class _GeneralDataSet(Dataset):
 
     def __init__(self, utt, sent, act, adj, adj_full, adj_id):
+
         self._utt = utt
         self._sent = sent
         self._act = act
@@ -151,6 +168,7 @@ class _GeneralDataSet(Dataset):
         self._adj_id = adj_id
 
     def __getitem__(self, item):
+
         return self._utt[item], self._sent[item], self._act[item],\
                self._adj[item], self._adj_full[item], self._adj_id[item]
 
@@ -159,6 +177,7 @@ class _GeneralDataSet(Dataset):
 
 
 def _collate_func(instance_list):
+
     """
     As a function parameter to instantiate the DataLoader object.
     """
@@ -167,6 +186,9 @@ def _collate_func(instance_list):
     scatter_b = [[] for _ in range(0, n_entity)]
 
     for idx in range(0, len(instance_list)):
+
         for jdx in range(0, n_entity):
+            
             scatter_b[jdx].append(instance_list[idx][jdx])
+
     return scatter_b
