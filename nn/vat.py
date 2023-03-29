ab63a0136741102cf2243336f54b7923d5a042d7
tazeek <tazeek.rakib@gmail.com>
2023-03-29 13:19:27 +0800

    Order

--- nn/vat.py
+++ nn/vat.py
@@ -23,14 +23,14 @@ def perform_vat(model, perturbation_level, utt_list, adj_list, adj_full_list, ad
     # Preprocess the data, first and foremost
     var_utt, var_p, mask, len_list, _, var_adj, var_adj_full, var_adj_R = \
             model.preprocess_data(utt_list, adj_list, adj_full_list, adj_id_list)
+    
+    # Get the original logits
 
     # Define the level of perturbation (See Canva document)
     # Perform the necessary preprocessing (as per flow: See Canva document)
     if perturbation_level == "bilstm_layer":
         ...
 
-    # Get the original logits
-    
     # Create random tensor and normalize with L2
 
     # Get the first KL Div loss (this is on the random tensor)
