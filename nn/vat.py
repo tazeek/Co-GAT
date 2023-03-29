56fd175b7e6a61cf49a498bc73273920a8340af2
tazeek <tazeek.rakib@gmail.com>
2023-03-29 12:03:06 +0800

    VAT function added

---
+++ nn/vat.py
@@ -0,0 +1,19 @@
+def create_random_tensor():
+
+    ...
+
+def get_original_logits():
+
+    ...
+
+def get_kl_div_loss():
+
+    ...
+
+def update_gradients_perturbation():
+
+    ...
+
+def perform_vat():
+
+    ...
